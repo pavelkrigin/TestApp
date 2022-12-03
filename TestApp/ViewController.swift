3c2a85cde89f8812651d14737c0db09b8a2d8b55
Pavel Krigin <pavel.krigin.85@gmail.com>
2022-12-03 18:01:36 +0300

    Initial Commit

---
+++ TestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestApp
+//
+//  Created by Pavel Krigin on 3.12.22..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
